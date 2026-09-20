c4cf8261248f5dcf9895bbb2e51f875484003903
plikhk <alikhanmegacoolboy@gmail.com>
2026-09-20 20:07:23 +0500

    doin' things

---
+++ src/gui/WindowsFactory.java
@@ -0,0 +1,12 @@
+package gui;
+
+public class WindowsFactory implements GUIFactory{
+    @Override
+    public Button createButton(){
+        return new WindowsButton();
+    }
+    @Override
+    public Checkbox createCheckbox(){
+        return new WindowsCheckbox();
+    }
+}
